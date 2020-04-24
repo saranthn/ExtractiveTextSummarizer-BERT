58f698c810c92cbf224c2fcf8b724a957d253ef7
anirudhs1998 <anirudhs1998@gmail.com>
2020-04-24 14:17:49 +0530

    Added WCSS and BCSS to cluster.py

--- cluster.py
+++ cluster.py
@@ -4,26 +4,37 @@ import numpy as np
 from numpy import ndarray
 
 def cluster_features(features, ratio: float = 0.2) -> List[int]:
-    """
-    Clusters sentences based on the ratio
-    :param ratio: Ratio to use for clustering
-    :return: Sentences index that qualify for summary
-    """
+        """
+        Clusters sentences based on the ratio
+        :param ratio: Ratio to use for clustering
+        :return: Sentences index that qualify for summary
+        """
 
-    k = 1 if ratio * len(features) < 1 else int(len(features) * ratio)
-    model = get_model(k).fit(features)
-    centroids = get_centroids(model)
-    cluster_args = find_closest_args(centroids, features)
-    sorted_values = sorted(cluster_args.values())
-    return sorted_values
+        k = 1 if ratio * len(features) < 1 else int(len(features) * ratio)
+        model = get_model(k).fit(features)   
+        centroids = get_centroids(model)          
+        #labels = get_labels(model)
+        cluster_args = find_closest_args(centroids, features)  
+        wcss_distance = within_cluster_ss(centroids,k,model,features)
+        bcss_distance = between_cluster_ss(centroids)
+        sorted_values = sorted(cluster_args.values())
+        return sorted_values
 
 def get_model(k: int):
+        """
+        Retrieve clustering model
+        :param k: amount of clusters
+        :return: Clustering model
+        """
+        return KMeans(n_clusters=k, random_state=12345)
+    
+def get_labels(model):
     """
-    Retrieve clustering model
-    :param k: amount of clusters
-    :return: Clustering model
+    Retrieve labels of points
+    :param model: Clustering model
+    :return: Labels
     """
-    return KMeans(n_clusters=k, random_state=12345)
+    return model.labels_
 
 def get_centroids(model):
     """
@@ -60,3 +71,59 @@ def find_closest_args(centroids: np.ndarray, features):
         cur_arg = -1
 
     return args
+
+
+
+def within_cluster_ss(centroids:np.ndarray,k,model,features):
+    """
+    Find the WCSS
+    :param centroids: Centroids of the each cluster, k:n_cluster, model: Clustering Model, features: All points 
+    :return: WCSS of the clusters
+    """
+    cluster = {i: np.where(model.labels_ == i)[0] for i in range(k)}  #Each cluster and indices in that cluster
+    
+    for i in range(len(cluster)):
+        cluster2 = []
+        for point in cluster[i]:
+            cluster2.append(features[point])
+            cluster[i] = cluster2
+        
+    '''
+    cluster[i] has list of points which have centroid as i
+    '''
+        
+    '''
+    (cluster[0][i] - centroid[0]) distance
+    '''    
+
+    wcss = []
+    for j,centroid in enumerate(centroids):
+        centroid_dist = 0  #Sum of euclidean distances of points in each cluster
+        for point in cluster[j]:
+            distance = np.linalg.norm(point - centroid, 2)
+            centroid_dist = centroid_dist + distance
+        wcss.append(centroid_dist)    
+    
+  
+    return wcss
+
+def between_cluster_ss(centroids:np.ndarray):
+    """
+    Find the BCSS
+    :param: centroids: Centroids of each cluster
+    :return: BCSS of the clustering
+    """
+    bcss = []
+    for j,centroid in enumerate(centroids):
+        current_centroid = centroid
+        current_distance = 0
+        for i,new_centroid in enumerate(centroids):
+            distance = np.linalg.norm(current_centroid - new_centroid, 2)
+            current_distance = current_distance + distance
+        bcss.append(current_distance)
+    
+    
+    return bcss
+            
+            
+    
