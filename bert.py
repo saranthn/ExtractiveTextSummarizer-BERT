1d0f9fce0d0c42234b16ed7450df9b856093a434
Ashwin Shankar <ashwinshankar5@gmail.com>
2020-04-26 13:47:51 +0530

    updated the rouge test part (modularized)

--- bert.py
+++ bert.py
@@ -3,81 +3,36 @@ import pickle
 with open('cnn_dataset.pkl', 'rb') as f:
     data = pickle.load(f)
 
-from sklearn.cluster import KMeans
-from typing import List
-import numpy as np
-from numpy import ndarray
-import torch
-from transformers import BertTokenizer, BertModel, BertConfig
 import logging
 from cluster import *
 from sentence_handler import *
+from rouge import Rouge
+
+#from coreference import *
 from coreference import *
 from rouge_test import *
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-# Load pre-trained model (weights)
-model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
-
-# Put the model in "evaluation" mode, meaning feed-forward operation.
-model.eval()
-
-def extract_embeddings(sentence) -> ndarray:
-    # Tokenize our sentence with the BERT tokenizer.
-    tokenized_text = tokenizer.tokenize(sentence)
-
-    # Map the token strings to their vocabulary indeces.
-    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-
-    # Print out the tokens.
-    #print (tokenized_text)
-
-    # Display the words with their indices.
-    #for tup in zip(tokenized_text, indexed_tokens):
-    #    print('{:<12} {:>6,}'.format(tup[0], tup[1]))
-
-    segments_ids = [1] * len(tokenized_text)
-
-    # Convert inputs to PyTorch tensors
-    tokens_tensor = torch.tensor([indexed_tokens])
-    segments_tensors = torch.tensor([segments_ids])
-
-    with torch.no_grad():
-        outputs = model(tokens_tensor)
-
-    #Sequence of hidden-states at the last layer of the model.
-    #torch.FloatTensor of shape (batch_size, sequence_length, hidden_size)
-    last_hidden_states = outputs[0]
-
-    #Last layer hidden-state of the first token of the sequence (classification token) further processed by a Linear layer and a Tanh activation function.
-    #This output is usually not a good summary of the semantic content of the input, It is better with averaging or pooling the sequence of hidden-states for the whole input sequence.
-    #torch.FloatTensor: of shape (batch_size, hidden_size)
-    pooler_output = outputs[1]
-
-    #Hidden-states of the model at the output of each layer plus the initial embedding outputs.
-    #torch.FloatTensor (one for the output of the embeddings + one for the output of each layer) of shape (batch_size, sequence_length, hidden_size)
-    hidden_states = outputs[2]
-
-    embedding_output = hidden_states[0]
-    attention_hidden_states = hidden_states[1:]
-
-    pooled = hidden_states[-2].mean(dim=1)
-
-    return pooled
-
 def create_matrix(content) -> ndarray:
-    return np.asarray([
-        np.squeeze(extract_embeddings(t).data.numpy())
         for t in content
     ])
 
+def run_clusters(content, ratio=0.5, algorithm='kmeans') -> List[str]:
+    print(content)
+    #referenced_data = coreference_handler(content)
+    #print(referenced_data)
 def run_clusters(content, ratio=0.3, algorithm='kmeans') -> List[str]:
     referenced_data = coreference_handler(content)
     processed_sentences = sentence(content)
+    print(processed_sentences)
     features = create_matrix(processed_sentences)
     hidden_args = cluster_features(features, ratio)
     return [content[j] for j in hidden_args]
+
+#sentences_summary = run_clusters(data[876]['story'],0.5,'kmeans')
+#summary = ' '.join(sentences_summary)
+
 
 for i in range(1,5):
     sentences_summary = run_clusters(data[i]['story'],0.3,'kmeans')
--- rouge_test.py
+++ rouge_test.py
@@ -1,14 +1,35 @@
-from rouge import Rouge
+from sumeval.metrics.rouge import RougeCalculator
 
 def rouge_score(gen_summary, gold_summary):
     gold_summary_formatted = ""
     for i in range(len(gold_summary)):
-        gold_summary_formatted += "[CLS] " + gold_summary[i] + " [SEP]. " 
+        gold_summary_formatted += "[CLS] " + gold_summary[i] + " [SEP]. "
     rouge = Rouge()
     print("##################")
     print(gold_summary_formatted)
     print("##################")
     print(gen_summary)
     print("##################")
-    scores = rouge.get_scores(gen_summary, gold_summary_formatted)
-    return scores
+
+    rouge = RougeCalculator(stopwords=True, lang="en")
+
+    rouge_1 = rouge.rouge_n(
+                summary=gen_summary,
+                references=gold_summary_formatted,
+                n=1)
+
+    rouge_2 = rouge.rouge_n(
+                summary=gen_summary,
+                references=gold_summary_formatted,
+                n=2)
+
+    rouge_l = rouge.rouge_l(
+                summary=gen_summary,
+                references=gold_summary_formatted)
+
+    rouge_be = rouge.rouge_be(
+                summary=gen_summary,
+                references=gold_summary_formatted)
+
+    scores={"ROUGE-1":rouge_1,"ROUGE-2":rouge_2,"ROUGE-L":rouge_l,"ROUGE-BE":rouge_be}
+    return scores 
